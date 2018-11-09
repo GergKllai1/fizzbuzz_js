a70d8021efefd55aa5ae7d39fc7939f9dd33e271
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-09 15:33:18 +0100

    adds FizzBuzz class with return number functionality

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -2,8 +2,8 @@ const {FizzBuzz} = require('./spec.helper.js')
 
 describe('Fizz Buzz', () => {
     let fizzBuzz = new FizzBuzz
-});
 
-it('returns a number if no game rules are met', () =>{
-    expect(fizzBuzz.check(1)).to.eql(1)
+    it('returns a number if no game rules are met', () =>{
+        expect(fizzBuzz.check(1)).to.eql(1)
+    });
 });
---
+++ src/js/fizz_buzz.js
@@ -0,0 +1,5 @@
+class FizzBuzz {
+    check(number){
+        return number;
+    }
+};
