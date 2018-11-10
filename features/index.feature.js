7125bcc25c8c27da2250c014ab8001f168f21dbd
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-10 15:41:09 +0100

    adds feature test and index.html

--- features/index.feature.js
+++ features/index.feature.js
@@ -7,4 +7,20 @@ describe('User can input a value and get FizzBuzz results', () => {
         await browser.init()
         await browser.visitPage('http://localhost:8080/')
     });
+
+    beforeEach(async () => {
+        await browser.page.reload();
+    });
+
+    after(async () => {
+        await browser.close();
+    })
+
+    it('clicking on "Check" button', async () => {
+        await browser.fillIn("input[id='value']", { with: '3'})
+        await browser.clickOnButton("input[value='Check']")
+        let content = await browser.getContent("[id='display_anwser']")
+        expect(content).to.eql('Fizz');
+    })
+
 })
