7bb2a62db77dd7ae8892e6c9feb49610dbef8aa3
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-10 12:27:43 +0100

    adds fizzbuzz code, unit tests are passing

--- src/js/fizz_buzz.js
+++ src/js/fizz_buzz.js
@@ -1,5 +1,13 @@
 class FizzBuzz {
-    check(number){
-        return number;
+    check(number) {
+        if(number % 15 === 0) {
+            return 'FizzBuzz'
+        } else if(number % 5 === 0) {
+            return 'Buzz'
+        } else if(number % 3 === 0) {
+            return 'Fizz'
+        } else {
+            return number
+        }
     }
-};
+}
