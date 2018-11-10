a98e43211b813f173e2b1980c0f3a10b0528fc70
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-10 22:09:09 +0100

    adds feature for user to check whether the value is fizz or buzz or fizzbuzz

--- src/js/fizz_buzz.js
+++ src/js/fizz_buzz.js
@@ -1,6 +1,8 @@
 class FizzBuzz {
     check(number) {
-        if(number % 15 === 0) {
+        if(number === ''){
+            return 'Please choose a number'
+        } else if(number % 15 === 0) {
             return 'FizzBuzz'
         } else if(number % 5 === 0) {
             return 'Buzz'
@@ -11,3 +13,51 @@ class FizzBuzz {
         }
     }
 }
+
+const randomizer = document.addEventListener('DOMContentLoaded', () => {
+    let randomInput = document.getElementById('random')
+    let randomGenerator = function () {
+        return Math.floor(Math.random() * 10)
+    }
+    let generator = document.getElementById('generator')
+    generator.addEventListener('click', () => {
+        randomInput.innerHTML = randomGenerator();
+    })
+})
+
+const solution = document.addEventListener('DOMContentLoaded', () => {
+    let fizz = document.getElementById('fizz');
+    let buzz = document.getElementById('buzz');
+    let fizzbuzz = document.getElementById('fizzbuzz');
+    let fizzBuzz = new FizzBuzz
+    fizz.addEventListener('click', () => {
+        let value = document.getElementById('random').innerHTML
+        let anwser = fizzBuzz.check(value);
+        if(anwser === "Fizz"){
+            alert("Congratulations you answered correctly!")
+        }else{
+            alert('You have lost, try again!')
+        };
+        
+    });
+    buzz.addEventListener('click', () => {
+        let value = document.getElementById('random').innerHTML
+        let anwser = fizzBuzz.check(value);
+        if(anwser === "Buzz"){
+            alert("Congratulations you answered correctly!")
+        }else{
+            alert('You have lost, try again!')
+        };
+        
+    });
+    fizzbuzz.addEventListener('click', () => {
+        let value = document.getElementById('random').innerHTML
+        let anwser = fizzBuzz.check(value);
+        if(anwser === "FizzBuzz"){
+            alert("Congratulations you answered correctly!")
+        }else{
+            alert('You have lost, try again!')
+        };;
+        
+    })
+})
