14611967403763bae5baa7339af2362f92e3e16b
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-10 14:01:04 +0100

    adds features test file

---
+++ features/index.feature.js
@@ -0,0 +1,10 @@
+const { FizzBuzz } = require('../spec/spec.helper')
+const BrowserHelpers = require ('e2e_training_wheels')
+const browser = new BrowserHelpers()
+
+describe('User can input a value and get FizzBuzz results', () => {
+    before(async() => {
+        await browser.init()
+        await browser.visitPage('http://localhost:8080/')
+    });
+})
